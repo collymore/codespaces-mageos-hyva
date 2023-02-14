b9c5504a48540674dfae4cb473114fb19a2e6a8a
Raphael Ola <follypimpz@gmail.com>
2023-02-14 11:56:34 +0000

    Adding Skeletal Punchout Module

---
+++ develo-punchout/app/code/Develodesign/Punchout/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Develodesign_Punchout', __DIR__);
+
