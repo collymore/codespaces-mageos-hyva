c002c4850bafb3a8ea4e59a3d374bba65be6d78e
Raphael Ola <follypimpz@gmail.com>
2023-04-16 00:11:10 +0100

    Generating cxml data

--- view/adminhtml/web/js/search.js
+++ view/adminhtml/web/js/search.js
@@ -30,12 +30,41 @@ define([
             );
         }
         else if($(this).hasClass("cxml")){
+            const xml = $($.parseXML('<?xml version="1.0" encoding="UTF-8"?>' +
+                '<!DOCTYPE cXML SYSTEM "http://xml.cxml.org/schemas/cXML/1.0/cXML.dtd">' +
+                '<cXML payloadID="'+ $(this).parent(".punchout-options").find(".cxml-payload-id").val()+'" timestamp="2019-08-03T02:06:05+00:00" version="1.2.044"><Header>\n' +
+                '        <From>\n' +
+                '            <Credential domain="NetworkId">\n' +
+                '                <Identity>218171296</Identity>\n' +
+                '            </Credential>\n' +
+                '        </From>\n' +
+                '        <To>\n' +
+                '            <Credential domain="DUNS">\n' +
+                '                <Identity>acme</Identity>\n' +
+                '            </Credential>\n' +
+                '        </To>\n' +
+                '        <Sender>\n' +
+                '            <Credential domain="NetworkId">\n' +
+                '                <Identity>'+ $(this).parent(".punchout-options").find(".cxml-sender-identity").val() + '</Identity>\n' +
+                '                <SharedSecret>'+$(this).parent(".punchout-options").find(".cxml-shared-secret").val()+'</SharedSecret>\n' +
+                '            </Credential>\n' +
+                '            <UserAgent>Application Name v1.2.3</UserAgent>\n' +
+                '        </Sender>\n' +
+                '    </Header>' +
+                '<Request deploymentMode="production">\n' +
+                '        <PunchOutSetupRequest operation="create">\n' +
+                '            <BuyerCookie>'+$(this).parent(".punchout-options").find(".cxml-buyer-cookie").val()+'</BuyerCookie>' +
+
+                '</cXML>'));
+
             $.ajax({
-                type: "GET",
+                type: "POST",
                 url: $(this).parent(".punchout-options").find(".cxml-url").val(),
                 cache: false,
                 dataType: "xml",
+                contentType: "text/xml",
                 data: {
+
                     'testpunchout': "testpunchout",
                     'payloadId': $(this).parent(".punchout-options").find(".cxml-payload-id").val(),
                     'buyer_cookie': $(this).parent(".punchout-options").find(".cxml-buyer-cookie").val(),
@@ -48,7 +77,7 @@ define([
                     'return_url': $(this).parent(".punchout-options").find(".cxml-return-url").val()
                 },
                 success: function(data) {
-                    var returnUrl = $(data).find('URL').first().text();
+                    const returnUrl = $(data).find('URL').first().text();
                     if(returnUrl){
                         window.open(returnUrl, '_blank');
                     }
